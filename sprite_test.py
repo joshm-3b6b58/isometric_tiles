3e85122f38d0b655afaf623f9b4106bce8812b30
joshmutus <josh.mutus@gmail.com>
2025-09-18 11:20:48 +0100

    Creating an array of tiles that can be selected and disselected by a cursor.

--- sprite_test.py
+++ sprite_test.py
@@ -1,13 +1,17 @@
-from main import LandTile
-import arcade  
+from main import LandTile, SiteModel
+import arcade
+
 
 def test_land_tile():
     """Test that the land tile functionality."""
     dt_path = "grid_cell.png"
     ht_path = "selected_grid_cell.png"
-    sprite = LandTile(dt_path, ht_path)
+    site = (20, 31)
+    sprite = LandTile(dt_path, ht_path, site)
     default_texture = arcade.texture.default_texture_cache.load_or_get_texture(dt_path)
-    hilite_texture =  arcade.texture.default_texture_cache.load_or_get_texture(ht_path)
+    hilite_texture = arcade.texture.default_texture_cache.load_or_get_texture(ht_path)
     assert sprite.texture == default_texture
-    sprite.change_texture()
+    assert sprite.collide_cursor() == site
     assert sprite.texture == hilite_texture
+    assert sprite.un_collide_cursor() == site
+    assert sprite.texture == default_texture
