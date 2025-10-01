217a01196d2a371795b5a6d93c2b45a55984ba8f
joshm-hash <8173925+joshm-3b6b58@users.noreply.github.com>
2025-10-01 10:11:31 -0700

    Fix tests.

--- tests/test_sprites.py
+++ tests/test_sprites.py
@@ -7,8 +7,8 @@ from grid_view.custom_sprites import LandTile
 
 def test_land_tile():
     """Test that the land tile functionality."""
-    dt_path = "grid_cell.png"
-    ht_path = "selected_grid_cell.png"
+    dt_path = "assets/grid_cell.png"
+    ht_path = "assets/selected_grid_cell.png"
     site = (20, 31)
     sprite = LandTile(dt_path, ht_path, site)
     default_texture = arcade.texture.default_texture_cache.load_or_get_texture(dt_path)
